3a7373c7703246e1aa32093548d7a4151936f1cd
aaron <aaron@MacBook-Pro.local>
2020-05-31 17:40:00 -0700

    Chat App Project - Added Acknowledgment functionality and Chat Template features.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,61 @@
 const socket = io();
 
+const $messageForm = document.querySelector('#message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#send-location');
+const $messages = document.querySelector('#messages');
+
+
+//Template(s)
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector('#location-template').innerHTML;
 
 socket.on('message', (message) => {
     console.log(message);
+    const html = Mustache.render(messageTemplate, { 
+        message: message
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+socket.on("locationMessage", (mapsUrl) => {
+    console.log(mapsUrl);
+    const html = Mustache.render(locationTemplate, {
+        url: mapsUrl
+    })
+});
+
+$messageForm.addEventListener('submit', (e) => {
     //Prevents full Web Browser Refresh
     e.preventDefault();
 
-    const message = e.target.elements.message.value; //document.querySelector('input').value;
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
-    socket.emit('sendMessage', message);
+    const message = e.target.elements.message.value; //document.querySelector('input').value;
+    
+    socket.emit('sendMessage', message, function(ack){
+        $messageFormButton.removeAttribute('disabled', 'disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+    });
+});
+
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.');
+    } else {
+        $sendLocationButton.setAttribute('disabled', 'disabled');
+        navigator.geolocation.getCurrentPosition((position) => {
+            socket.emit('sendLocation', {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            }, function(ack) {
+                $sendLocationButton.removeAttribute('disabled', 'disabled');
+                console.log(ack);
+            });
+        });
+    }
+   
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const http = require('http');
-const express = require('express');
 const socketio = require('socket.io');
+const express = require('express');
 
 
 const app = express();
@@ -13,16 +13,24 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-//server(emit) -> client (receive) 
-//client(emit) -> server (receive)
+//server(emit) -> client (receive) --acknowledgement (optional) -> Server
+//client(emit) -> server (receive) --acknowledgement (optional) -> client
 io.on('connection', (socket) => {
     console.log("New WebSocket Connection")
 
     socket.emit('message', 'Hello!');
     socket.broadcast.emit('message', 'New User Joined')
 
-    socket.on('sendMessage', (message) => {
+    //Receive sendMessage on Server and when fired send message to all connected clients.
+    socket.on('sendMessage', (message, ack) => {
         io.emit('message', message);
+        ack();
+    });
+
+    //Receive sendLocation on Server
+    socket.on('sendLocation', function(location, ack) {
+        io.emit('locationMessage', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+        ack('Your Location was Shared');
     });
 
     socket.on('disconnect', () => {
